34c0d46ab3cec37440796350a60bea8652772ba1
heki5656 <heki5656@colorado.edu>
2022-11-14 19:34:52 -0700

    fixed camera

--- KimPanJaishankarProject1/Assets/FallenChairPopUpWindow.cs
+++ KimPanJaishankarProject1/Assets/FallenChairPopUpWindow.cs
@@ -1,77 +1,77 @@
-using UnityEngine;
-using UnityEditor;
+// using UnityEngine;
+// using UnityEditor;
 
-public class ShowPopupExample : EditorWindow
-{
-    [MenuItem("Example/ShowPopup Example")]
-    static void Init()
-    {
-        ShowPopupExample window = ScriptableObject.CreateInstance<ShowPopupExample>();
-        window.position = new Rect(Screen.width / 2, Screen.height / 2, 250, 150);
-        window.ShowPopup();
-    }
+// public class ShowPopupExample : EditorWindow
+// {
+//     [MenuItem("Example/ShowPopup Example")]
+//     static void Init()
+//     {
+//         ShowPopupExample window = ScriptableObject.CreateInstance<ShowPopupExample>();
+//         window.position = new Rect(Screen.width / 2, Screen.height / 2, 250, 150);
+//         window.ShowPopup();
+//     }
 
-    void OnGUI()
-    {
-        EditorGUILayout.LabelField("Fallen chair was found at the scene of the crime, forensics have verified that the victime was sitting on it before their death.", EditorStyles.wordWrappedLabel);
-        GUILayout.Space(70);
-        if (GUILayout.Button("close")) this.Close();
-    }
-}
+//     void OnGUI()
+//     {
+//         EditorGUILayout.LabelField("Fallen chair was found at the scene of the crime, forensics have verified that the victime was sitting on it before their death.", EditorStyles.wordWrappedLabel);
+//         GUILayout.Space(70);
+//         if (GUILayout.Button("close")) this.Close();
+//     }
+// }
 
-/*using UnityEngine;
-using UnityEditor;
+// /*using UnityEngine;
+// using UnityEditor;
 
-public class EditorWindowWithPopup : EditorWindow
-{
-    // Add menu item
-    [MenuItem("Example/Popup Example")]
-    static void Init()
-    {
-        EditorWindow window = EditorWindow.CreateInstance<EditorWindowWithPopup>();
-        window.Show();
-    }
+// public class EditorWindowWithPopup : EditorWindow
+// {
+//     // Add menu item
+//     [MenuItem("Example/Popup Example")]
+//     static void Init()
+//     {
+//         EditorWindow window = EditorWindow.CreateInstance<EditorWindowWithPopup>();
+//         window.Show();
+//     }
 
-    Rect buttonRect;
-    void OnGUI()
-    {
-        {
-            GUILayout.Label("Fallen Chair found at crime scene", EditorStyles.boldLabel);
-            if (GUILayout.Button("Press for more information", GUILayout.Width(200)))
-            {
-                PopupWindow.Show(buttonRect, new PopupExample());
-            }
-            if (Event.current.type == EventType.Repaint) buttonRect = GUILayoutUtility.GetLastRect();
-        }
-    }
-}
+//     Rect buttonRect;
+//     void OnGUI()
+//     {
+//         {
+//             GUILayout.Label("Fallen Chair found at crime scene", EditorStyles.boldLabel);
+//             if (GUILayout.Button("Press for more information", GUILayout.Width(200)))
+//             {
+//                 PopupWindow.Show(buttonRect, new PopupExample());
+//             }
+//             if (Event.current.type == EventType.Repaint) buttonRect = GUILayoutUtility.GetLastRect();
+//         }
+//     }
+// }
 
-public class PopupExample : PopupWindowContent
-{
-    bool toggle1 = true;
-    bool toggle2 = true;
-    bool toggle3 = true;
+// public class PopupExample : PopupWindowContent
+// {
+//     bool toggle1 = true;
+//     bool toggle2 = true;
+//     bool toggle3 = true;
 
-    public override Vector2 GetWindowSize()
-    {
-        return new Vector2(200, 150);
-    }
+//     public override Vector2 GetWindowSize()
+//     {
+//         return new Vector2(200, 150);
+//     }
 
-    public override void OnGUI(Rect rect)
-    {
-        GUILayout.Label("Popup Options Example", EditorStyles.boldLabel);
-        toggle1 = EditorGUILayout.Toggle("Toggle 1", toggle1);
-        toggle2 = EditorGUILayout.Toggle("Toggle 2", toggle2);
-        toggle3 = EditorGUILayout.Toggle("Toggle 3", toggle3);
-    }
+//     public override void OnGUI(Rect rect)
+//     {
+//         GUILayout.Label("Popup Options Example", EditorStyles.boldLabel);
+//         toggle1 = EditorGUILayout.Toggle("Toggle 1", toggle1);
+//         toggle2 = EditorGUILayout.Toggle("Toggle 2", toggle2);
+//         toggle3 = EditorGUILayout.Toggle("Toggle 3", toggle3);
+//     }
 
-    public override void OnOpen()
-    {
-        Debug.Log("Popup opened: " + this);
-    }
+//     public override void OnOpen()
+//     {
+//         Debug.Log("Popup opened: " + this);
+//     }
 
-    public override void OnClose()
-    {
-        Debug.Log("Popup closed: " + this);
-    }
-}*/
+//     public override void OnClose()
+//     {
+//         Debug.Log("Popup closed: " + this);
+//     }
+// }*/
---
+++ KimPanJaishankarProject1/testandroid_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,117 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void BreakableWindow::Start()
+extern void BreakableWindow_Start_m96D4AC4F5DB943824FCA74B23F20D8667FE19FA7 (void);
+// 0x00000002 System.Void BreakableWindow::bakeVertices(System.Boolean)
+extern void BreakableWindow_bakeVertices_m8844D308B1398B0103EA5473D481DF07DC414C3A (void);
+// 0x00000003 System.Void BreakableWindow::generateSingleSplinter(System.Int32[],UnityEngine.Transform)
+extern void BreakableWindow_generateSingleSplinter_mE293D225A81F575ECD3F07AEB254E04636C7DC16 (void);
+// 0x00000004 System.Void BreakableWindow::bakeSplinters()
+extern void BreakableWindow_bakeSplinters_m856D2E33D1EE42D70AC9319116F116D1104196FD (void);
+// 0x00000005 UnityEngine.GameObject[] BreakableWindow::breakWindow()
+extern void BreakableWindow_breakWindow_m2145F2084AA48F048DD420D5D19A8180019C13FF (void);
+// 0x00000006 System.Void BreakableWindow::OnCollisionEnter(UnityEngine.Collision)
+extern void BreakableWindow_OnCollisionEnter_m666D6A2A1AB110C13A86DF97220CA20DEF00479D (void);
+// 0x00000007 System.Void BreakableWindow::.ctor()
+extern void BreakableWindow__ctor_m127AB296D605C614C944AABBA11A86A44CF993CC (void);
+// 0x00000008 System.Void BasicCardboardMovement::Start()
+extern void BasicCardboardMovement_Start_mE3117C34CD87D9F8E6B749CF14427A30021700CE (void);
+// 0x00000009 System.Void BasicCardboardMovement::Update()
+extern void BasicCardboardMovement_Update_m7177D2D1E576F44ED2FBA543FBABCBCD63570AA9 (void);
+// 0x0000000A System.Void BasicCardboardMovement::.ctor()
+extern void BasicCardboardMovement__ctor_m97CAE2692CFB2B36D94A9F5DCEF3E70C9B001AF3 (void);
+// 0x0000000B System.Void IntroTextFade::Start()
+extern void IntroTextFade_Start_m6213E06018F7E5898100FD6C9161D03DE8AF2010 (void);
+// 0x0000000C System.Collections.IEnumerator IntroTextFade::LerpFunction(UnityEngine.Color,System.Single)
+extern void IntroTextFade_LerpFunction_m2062AF3E8378963D82CF2F470F5605619985401B (void);
+// 0x0000000D System.Void IntroTextFade::.ctor()
+extern void IntroTextFade__ctor_m2AE7AFDEF478D92352C0ED40CE7062DC74F4A0AC (void);
+// 0x0000000E System.Void IntroTextFade/<LerpFunction>d__3::.ctor(System.Int32)
+extern void U3CLerpFunctionU3Ed__3__ctor_m7AA7E77CCA4F2CE353FFF96B9E9FC9AF0A61FD5B (void);
+// 0x0000000F System.Void IntroTextFade/<LerpFunction>d__3::System.IDisposable.Dispose()
+extern void U3CLerpFunctionU3Ed__3_System_IDisposable_Dispose_mAA2B9826FBF1891230E80B594B11CF912A9DF686 (void);
+// 0x00000010 System.Boolean IntroTextFade/<LerpFunction>d__3::MoveNext()
+extern void U3CLerpFunctionU3Ed__3_MoveNext_m6A6DEA9E2AD8FE7DF9A34AA2752FA5F4E7D0528A (void);
+// 0x00000011 System.Object IntroTextFade/<LerpFunction>d__3::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CLerpFunctionU3Ed__3_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mA0BD2532C21A319908D48B19296D3A55315870DF (void);
+// 0x00000012 System.Void IntroTextFade/<LerpFunction>d__3::System.Collections.IEnumerator.Reset()
+extern void U3CLerpFunctionU3Ed__3_System_Collections_IEnumerator_Reset_m193C27862AF4229F7AA3F710149E10CBE955FB26 (void);
+// 0x00000013 System.Object IntroTextFade/<LerpFunction>d__3::System.Collections.IEnumerator.get_Current()
+extern void U3CLerpFunctionU3Ed__3_System_Collections_IEnumerator_get_Current_mDA32F25AC444DFB9B6CE73D6105666DAD686F00C (void);
+static Il2CppMethodPointer s_methodPointers[19] = 
+{
+	BreakableWindow_Start_m96D4AC4F5DB943824FCA74B23F20D8667FE19FA7,
+	BreakableWindow_bakeVertices_m8844D308B1398B0103EA5473D481DF07DC414C3A,
+	BreakableWindow_generateSingleSplinter_mE293D225A81F575ECD3F07AEB254E04636C7DC16,
+	BreakableWindow_bakeSplinters_m856D2E33D1EE42D70AC9319116F116D1104196FD,
+	BreakableWindow_breakWindow_m2145F2084AA48F048DD420D5D19A8180019C13FF,
+	BreakableWindow_OnCollisionEnter_m666D6A2A1AB110C13A86DF97220CA20DEF00479D,
+	BreakableWindow__ctor_m127AB296D605C614C944AABBA11A86A44CF993CC,
+	BasicCardboardMovement_Start_mE3117C34CD87D9F8E6B749CF14427A30021700CE,
+	BasicCardboardMovement_Update_m7177D2D1E576F44ED2FBA543FBABCBCD63570AA9,
+	BasicCardboardMovement__ctor_m97CAE2692CFB2B36D94A9F5DCEF3E70C9B001AF3,
+	IntroTextFade_Start_m6213E06018F7E5898100FD6C9161D03DE8AF2010,
+	IntroTextFade_LerpFunction_m2062AF3E8378963D82CF2F470F5605619985401B,
+	IntroTextFade__ctor_m2AE7AFDEF478D92352C0ED40CE7062DC74F4A0AC,
+	U3CLerpFunctionU3Ed__3__ctor_m7AA7E77CCA4F2CE353FFF96B9E9FC9AF0A61FD5B,
+	U3CLerpFunctionU3Ed__3_System_IDisposable_Dispose_mAA2B9826FBF1891230E80B594B11CF912A9DF686,
+	U3CLerpFunctionU3Ed__3_MoveNext_m6A6DEA9E2AD8FE7DF9A34AA2752FA5F4E7D0528A,
+	U3CLerpFunctionU3Ed__3_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mA0BD2532C21A319908D48B19296D3A55315870DF,
+	U3CLerpFunctionU3Ed__3_System_Collections_IEnumerator_Reset_m193C27862AF4229F7AA3F710149E10CBE955FB26,
+	U3CLerpFunctionU3Ed__3_System_Collections_IEnumerator_get_Current_mDA32F25AC444DFB9B6CE73D6105666DAD686F00C,
+};
+static const int32_t s_InvokerIndices[19] = 
+{
+	3500,
+	2786,
+	1578,
+	3500,
+	3407,
+	2851,
+	3500,
+	3500,
+	3500,
+	3500,
+	3500,
+	1205,
+	3500,
+	2832,
+	3500,
+	3346,
+	3407,
+	3500,
+	3407,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	19,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
